2899b7f4bd4e34e2bf938c0557efe9f6cd4082e6
Jordan Jambazov <jordan.jambazov@gmail.com>
2014-02-25 15:59:53 +0200

    Adding the file with the shell injection vulnerability.

---
+++ main_with_shell_injection.py
@@ -0,0 +1,18 @@
+import subprocess
+from StringIO import StringIO
+from wsgiref.simple_server import make_server
+ 
+def application(environ, start_response):
+    path = environ.get('PATH_INFO')[1:]
+ 
+    stdout = StringIO()
+    out, err = subprocess.Popen(['fortune {} | cowsay'.format(path)], stdout=subprocess.PIPE, shell=True).communicate()
+    for line in out.splitlines():
+        print >> stdout, line
+    start_response("200 OK", [('Content-Type','text/plain')])
+    return [stdout.getvalue()]
+ 
+if __name__ == '__main__':
+    httpd = make_server('', 8000, application)
+    print "Starting cow server on port 8000"
+    httpd.serve_forever()
